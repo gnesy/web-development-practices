8fb66e07a78c9cf30a7276161c34a8ab6fed1189
gnesy <gen.alvarado00@gmail.com>
2025-07-16 14:38:03 -0400

    folder integration

---
+++ Curso Tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content:["./*.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+        // Usa el nombre que quieras para referirte a la fuente (ej. 'roboto')
+                rowdies: ['Rowdies', 'serif'],
+            },
+        },
+    },
+    plugins: [],
+}
